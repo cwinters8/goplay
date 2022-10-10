ee20ce72f9953d629fa38081dc41250f7f3658ea
Clark Winters <clwinters8@gmail.com>
2022-10-10 15:33:02 -0500

    saving WIP I guess

--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.19
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/fasthttp/websocket v1.5.0 // indirect
 	github.com/georgysavva/scany v1.2.0 // indirect
 	github.com/gofiber/fiber/v2 v2.37.1 // indirect
 	github.com/gofiber/template v1.7.1 // indirect
+	github.com/gofiber/websocket/v2 v2.0.25 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -18,6 +20,7 @@ require (
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.15.10 // indirect
+	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
 )
 
-// may need to wrap html with template.HTML() when sent to vue to replace the existing contents of an element
 func setup() error {
 	app := fiber.New(fiber.Config{
 		Views: html.New("templates", ".html"),
@@ -16,7 +16,7 @@ func setup() error {
 	// initially rendered html
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", fiber.Map{
-			// could pass an initial list of animals here
+			"Animals": []string{"Four", "Speck", "Hush"},
 		})
 	})
 
@@ -26,6 +26,22 @@ func setup() error {
 			"Animals": []string{"Percy", "Friday", "Shorty", "Frost"},
 		})
 	})
+
+	// websockets!
+	ws := app.Group("/ws")
+	ws.Use() // TODO: websocket middleware
+	ws.Get("/:id", websocket.New(func(c *websocket.Conn) {
+		for {
+			// mt (message type) could be either binary type or text (I think?) type
+			_, _, err := c.ReadMessage()
+			if err != nil {
+				break
+			}
+			// do something with msg
+			// for now, echo back to the user?
+		}
+		fmt.Printf("connection closed") // maybe pass the id param here
+	}))
 
 	return app.Listen(":3000")
 }
